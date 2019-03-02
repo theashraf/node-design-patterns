1cdabf40bbfd2595078c4c8d9fb5c95dbf3218b6
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-03-02 19:59:19 +0200

    add factory pattern

---
+++ src/creational/factory/Customer.js
@@ -0,0 +1,24 @@
+class Customer {
+  constructor(name) {
+    this._name = name;
+    this._shoppingList = [];
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get shoppingList() {
+    return this._shoppingList;
+  }
+
+  addItemToShoppingList(item) {
+    this._shoppingList.push(item);
+  }
+}
+
+module.exports = Customer;
---
+++ src/creational/factory/VIPCustomer.js
@@ -0,0 +1,16 @@
+const Customer = require("./Customer");
+
+class VIPCustomer extends Customer {
+  constructor(name, discount) {
+    super(name);
+    this._discount = discount;
+  }
+  set discount(value) {
+    this._discount = value;
+  }
+  get discount() {
+    return this._discount;
+  }
+}
+
+module.exports = VIPCustomer;
